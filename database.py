4ebc3f5bf6a730a15a17da914dcd6a8131b4c6ee
Hugo Cachon <hugo.cachon@hetic.net>
2021-03-14 21:30:46 +0100

    Menu + exception + function

--- database.py
+++ database.py
@@ -2,40 +2,63 @@ import sqlite3
 import hash
 
 # Connection to the database
-connection = sqlite3.connect('database.db')
+conn = sqlite3.connect('database.db')
 # Cursor to navigate and execute in the database
-cursor = connection.cursor()
+cursor = conn.cursor()
 
 # Creation of the table "users"
-cursor.execute("""CREATE TABLE users (
-         pseudo BLOB,
-         email TEXT,
-       password BLOB
-   )""")
+# cursor.execute("""CREATE TABLE users (
+#       pseudo BLOB,
+#       email TEXT,
+#       password BLOB
+#       )""")
 
 
 # Function to add a new user
-def new_user(pseudo, email, password, algorithm):
+def new_user():
+
+    pseudo = str(input("Pseudo: ")),
+    email = str(input("Email: ")),
+    password = str(input("Password: ")),
+    algorithm = str(input("Algorithm: "))
+
+    # Connection to the database
+    connection = sqlite3.connect('database.db')
+    # Cursor to navigate and execute in the database
+    cursor = connection.cursor()
     # Hashing the password and choosing the algorithm
     password = hash.hash_password(password, algorithm)
 
     # Adding user to the db and closing connection
     cursor.execute("INSERT INTO users VALUES (?,?,?)", (pseudo, email, password))
     connection.commit()
+    connection.close()
 
 
 # Function to delete a user taking as parameter the rowid
-def delete_user(rowid):
+def delete_user():
+
+    rowid = int(input("ID to delete of the DB"))
+
+    # Connection to the database
+    connection = sqlite3.connect('database.db')
+    # Cursor to navigate and execute in the database
+    cursor = connection.cursor()
     cursor.execute("DELETE from users WHERE rowid = (?)", str(rowid))
     connection.commit()
+    connection.close()
 
 
 # Function to display the db
 def display_db():
+    # Connection to the database
+    connection = sqlite3.connect('database.db')
+    # Cursor to navigate and execute in the database
+    cursor = connection.cursor()
     cursor.execute("SELECT rowid, * FROM users")
     users = cursor.fetchall()
-    print("ID" + "\tPSEUDO" + "\t\tMAIL " + "\t\t\t\t\t\tPASSWORD")
-    print("---" + "\t-------" + "\t\t--------- " + "\t \t\t\t\t---------")
+    print("ID" + "\tPSEUDO" + "\t\tMAIL " + "\t\t\t\t\tPASSWORD")
+    print("---" + "\t-------" + "\t\t--------- " + "\t\t\t\t---------")
     for user in users:
         print(user[0],  "| ",  user[1], "\t| ", user[2], "\t| ", user[3])
     connection.commit()
--- main.py
+++ main.py
@@ -2,9 +2,31 @@ import database
 
 
 def main():
-    # database.new_user("John", "John@Doe.com", "JohnDoePassword123!", "sha256")
-    # database.delete_user(1)
-    database.display_db()
+
+    options = int(input("1: Add User\n2: Delete User\n3: Check password\n4: Display Db"))
+
+    if options == 1:
+        try:
+            database.new_user()
+        except Exception as e:
+            print("ERROR:", e)
+        finally:
+            database.display_db()
+
+    if options == 2:
+        try:
+            database.delete_user()
+        except Exception as e:
+            print("ERROR:", e)
+        finally:
+            database.display_db()
+
+    if options == 3:
+        print("TODO")
+
+    if options == 4:
+        database.display_db()
+        main()
 
 
 if __name__ == "__main__":
